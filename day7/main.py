bd801471f866d65514248d3855493119f2d2ddf2
Jason T <jason@jasont.dev>
2022-12-31 20:10:40 +1000

    Day 7 refactored

--- day7/main.py
+++ day7/main.py
@@ -1,8 +1,7 @@
 import fileinput
 
 
-class Node:
-    total_size_under_limit = 0
+class FSNode:
     dir_sizes = []
 
     def __init__(self, name: str, parent=None, size=0):
@@ -11,17 +10,8 @@ class Node:
         self._name = name
         self._size = size
 
-    def __len__(self):
-        return len(self._children)
-
-    def __getitem__(self, item):
-        return self._children[item]
-
-    def __setitem__(self, key, value):
-        self._children[key] = value
-
     def add_child(self, name: str, size=0):
-        self._children.append(Node(name, self, size))
+        self._children.append(FSNode(name, self, size))
 
     def get_name(self):
         return self._name
@@ -35,25 +25,22 @@ class Node:
                 return child
         return None
 
-    def size(self):
+    def calc_dir_sizes(self):
         if len(self._children) == 0:
             return self._size
 
         total_size = 0
         for i in self._children:
-            total_size += i.size()
+            total_size += i.calc_dir_sizes()
 
-        if total_size <= 100000:
-            Node.total_size_under_limit += total_size
-
-        Node.dir_sizes.append(total_size)
+        FSNode.dir_sizes.append(total_size)
         return total_size
 
 
 file = fileinput.input('input')
 line = next(file)
 
-root = Node('root')
+root = FSNode('root')
 root.add_child('/')
 current = root
 
@@ -73,19 +60,27 @@ while line is not None:
 
     line = next(file, None)
 
-root.size()
-print(Node.total_size_under_limit)
-
-free_space = 70_000_000 - root.size()
-deficit = 30_000_000 - free_space
-dir_sizes = Node.dir_sizes
+total_size = root.calc_dir_sizes()
+dir_sizes = FSNode.dir_sizes
 dir_sizes.sort()
 
-first_greater_than_deficit = 0
-for i in dir_sizes:
-    if i > deficit:
-        first_greater_than_deficit = i
-        break
-print(deficit)
-print(first_greater_than_deficit)
 
+# Part 1
+
+def at_most_100000(number):
+    return number <= 100_000
+
+
+smaller_dirs = filter(at_most_100000, dir_sizes)
+print(sum(smaller_dirs))
+
+
+# Part 2
+
+def is_greater_than_deficit(number):
+    free_space = 70_000_000 - total_size
+    deficit = 30_000_000 - free_space
+    return number >= deficit
+
+
+print(next(filter(is_greater_than_deficit, dir_sizes)))
